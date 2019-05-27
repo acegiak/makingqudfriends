6798a06ca4d25fcd822cbc052da4d7c233819cc3
Ashton McAllan <acegiak@gmail.com>
2019-05-27 17:43:34 +0930

    added handy and treaded mods

---
+++ ModHandy.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Text;
+
+namespace XRL.World.Parts
+{
+	[Serializable]
+	public class acegiak_ModHandy : IModification
+	{
+
+		public acegiak_ModHandy()
+		{
+		}
+
+		public acegiak_ModHandy(int Tier)
+			: base(Tier)
+		{
+		}
+
+		public override void Configure()
+		{
+			WorksOnSelf = true;
+		}
+
+		public override void ApplyModification(GameObject Object)
+		{
+                Body partBody = Object.GetPart<Body>();
+                if(partBody !=null){
+                    BodyPart body2 = partBody.GetBody();
+                    BodyPart firstAttachedPart = body2.GetFirstAttachedPart("Back", 0, partBody, true);
+                    if(firstAttachedPart != null){
+
+                        body2.AddPartAt(firstAttachedPart,"Arm", 2).AddPart("Hand", 2, "MetalFist", "Hands");
+                        body2.AddPartAt(firstAttachedPart,"Arm", 1).AddPart("Hand", 1, "MetalFist", "Hands");
+                        body2.AddPartAt(firstAttachedPart,"Missile Weapon", 2);
+                        body2.AddPartAt(firstAttachedPart,"Missile Weapon", 1);
+				        body2.AddPartAt(firstAttachedPart,"Thrown Weapon");
+                        body2.AddPartAt(firstAttachedPart,"Hands", 0, null, null, "Hands");
+                    }else{
+
+                        body2.AddPart("Arm", 2).AddPart("Hand", 2, "MetalFist", "Hands");
+                        body2.AddPart("Arm", 1).AddPart("Hand", 1, "MetalFist", "Hands");
+                        body2.AddPart("Missile Weapon", 2);
+                        body2.AddPart("Missile Weapon", 1);
+				        body2.AddPart("Thrown Weapon");
+                        body2.AddPart("Hands", 0, null, null, "Hands");
+                    }
+                }
+		}
+
+		public override bool SameAs(IPart p)
+		{
+			return base.SameAs(p);
+		}
+
+		public override bool AllowStaticRegistration()
+		{
+			return true;
+		}
+
+		public override void Register(GameObject Object)
+		{
+			Object.RegisterPartEvent(this, "GetDisplayName");
+			Object.RegisterPartEvent(this, "GetShortDescription");
+			Object.RegisterPartEvent(this, "GetShortDisplayName");
+			base.Register(Object);
+		}
+
+		public override bool FireEvent(Event E)
+		{
+			if (E.ID == "GetShortDescription")
+			{
+				string str = "\n&CHandy: This robot has been equiped with arms and hands for holding and manipulating things.";
+				E.SetParameter("Postfix", E.GetStringParameter("Postfix") + str);
+			}
+			else if ((E.ID == "GetDisplayName" || E.ID == "GetShortDisplayName") && (!ParentObject.Understood() || !ParentObject.HasProperName))
+			{
+				E.GetParameter<StringBuilder>("Prefix").Append("handy ");
+			}
+			return base.FireEvent(E);
+		}
+	}
+}
---
+++ ModTreaded.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Text;
+
+namespace XRL.World.Parts
+{
+	[Serializable]
+	public class acegiak_ModTreaded : IModification
+	{
+
+		public acegiak_ModTreaded()
+		{
+		}
+
+		public acegiak_ModTreaded(int Tier)
+			: base(Tier)
+		{
+		}
+
+		public override void Configure()
+		{
+			WorksOnSelf = true;
+		}
+
+		public override void ApplyModification(GameObject Object)
+		{
+                Body partBody = Object.GetPart<Body>();
+                if(partBody !=null){
+                    BodyPart body2 = partBody.GetBody();
+                    body2.AddPart("Tread", 2);
+                    body2.AddPart("Tread", 1);
+                }
+		}
+
+		public override bool SameAs(IPart p)
+		{
+			return base.SameAs(p);
+		}
+
+		public override bool AllowStaticRegistration()
+		{
+			return true;
+		}
+
+		public override void Register(GameObject Object)
+		{
+			Object.RegisterPartEvent(this, "GetDisplayName");
+			Object.RegisterPartEvent(this, "GetShortDescription");
+			Object.RegisterPartEvent(this, "GetShortDisplayName");
+			Object.RegisterPartEvent(this, "GetMaxWeight");
+			base.Register(Object);
+		}
+
+		public override bool FireEvent(Event E)
+		{
+			if (E.ID == "GetShortDescription")
+			{
+				string str = "\n&CTreaded: This robot has been equiped with treads allowing them to carry heavy loads.";
+				E.SetParameter("Postfix", E.GetStringParameter("Postfix") + str);
+			}
+			else if ((E.ID == "GetDisplayName" || E.ID == "GetShortDisplayName") && (!ParentObject.Understood() || !ParentObject.HasProperName))
+			{
+				E.GetParameter<StringBuilder>("Prefix").Append("treaded ");
+			}
+            if (E.ID == "GetMaxWeight")
+			{
+				E.AddParameter("Weight", (int)Math.Floor((double)(int)E.GetParameter("Weight") * 100));
+				return true;
+			}
+			return base.FireEvent(E);
+		}
+	}
+}
